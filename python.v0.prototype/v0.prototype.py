09929537bb787c37f41c2ecb4256aac7bb72e0ff
AydenMe <monochromaticsilver@gmail.com>
2023-06-29 17:11:14 -0700

    Initial commit

---
+++ python.v0.prototype/v0.prototype.py
@@ -0,0 +1,35 @@
+#hi, this is our app ver 0
+#Data for cities from kaggle (edit later)
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+print(color.BOLD + "WAYS TO FIGHT CLIMATE CHANGE" + color.END)
+print(color.BOLD + "1. Switch to renewable or green energy sources!" + color.END)
+#code so when you click on switching to green energy sources 1. the reason is explained below
+print("Most of the world is still reliant on fossil fuels like gas, coal, and oil. Since the 70’s, our greenhouse gas emissions have increased by around 90%! \nFossil fuels and industrial production reliant on fossil fuels are estimated to account for 78% of our total greenhouse gas emission increase from 1970 to 2011. \nBecause fossil fuels are responsible for such a big part of our greenhouse gas emissions, it is important to start switching to more renewable and green energy sources for our daily needs. \nHere are some ways to do this!")
+print("- Using wind energy, electricity, ocean energy, hydro power, solar energy, geothermal energy.")
+print("- Reduce usage of fossil fuel powered vehicles and walk or bike or use non fossil fuel vehicles instead.")
+print("- Switching to renewable and green energy sources!")
+print("- Investing in green or more energy efficient things like LED lights or electrically powered cars.")
+print(color.BOLD + "2. Saving energy!! (Everyday ways to save energy at home)" + color.END)
+#paragrah below is hidden unless you click on title
+print("Over the years that we inhabited Earth, studies have shown how we’ve gotten worse. This too takes a toll on Earth. The climate dramatically changed in the late 1880’s. There is a reason for this. It has to do with dishwashers, dryers, washers, air conditioners, trains, cars and factories. Most of earth's energy is still reliant on fossil fuels with realse greenhouse gasses. Saving your energy can lessen these emmisions!")
+print("- Reducing our water usage is a great way to control your energy usage.")
+print("- Use cold water for washing clothes and maybe even showering.")
+print("- Consuming less. Producing products takes energy and is bad for our enviornment!")
+print("- Buying recyclable, reusable, or second hand items.")
+print("- Remember to recycle your things.")
+print("- Eating more vegetables! Plant based foods use less energy than meats and dairy. Eating more vegetables is good for your body and the environment!")
+print("- Turn off lights and devices when you're not using them.")
+print("- Open windows or use ceiling fans instead of using air conditioning.")
+print("- Hanging wet clothes instead of using a dryer.")
+print("- Switching to LED light bulbs.")
+print("- Donating your used items or unnecessary things that can be resold and reused.")
